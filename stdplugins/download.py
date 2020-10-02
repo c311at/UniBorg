0c4d81bdf6c33e437c935f357db6b2f788e9c3cd
unknown <byazade@yandex.com>
2020-10-02 14:45:34 +0300

    .

--- stdplugins/download.py
+++ stdplugins/download.py
@@ -73,22 +73,13 @@ async def _(event):
                 round(percentage, 2))
             estimated_total_time = downloader.get_eta(human=True)
             try:
-<< << << < HEAD
-                current_message = "trying to download\n"
-                current_message += f"URL: {url}\n"
-                current_message += f"File Name: {file_name}\n"
-                current_message += f"{progress_str}\n"
-                current_message += f"{humanbytes(downloaded)} of {humanbytes(total_length)}\n"
-                current_message += f"ETA: {estimated_total_time}"
-== == == =
                 current_message = f"trying to download\n"\
-                                  f"URL: {url}\n"\
-                                  f"File Name: {file_name}\n" \
-                                  f"Speed: {speed}"\
-                                  f"{progress_str}\n"\
-                                  f"{utils.humanbytes(downloaded)} of {utils.humanbytes(total_length)}\n"\
-                                  f"ETA: {estimated_total_time}"
->>>>>> > aea8912d89b5f605e52dde7c95e809162f6ec390
+                    f"URL: {url}\n"\
+                    f"File Name: {file_name}\n" \
+                    f"Speed: {speed}"\
+                    f"{progress_str}\n"\
+                    f"{utils.humanbytes(downloaded)} of {utils.humanbytes(total_length)}\n"\
+                    f"ETA: {estimated_total_time}"
                 if round(diff % 10.00) == 0 and current_message != display_message:
                     await mone.edit(current_message)
                     display_message = current_message
